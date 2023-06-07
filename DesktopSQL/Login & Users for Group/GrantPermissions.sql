f1490157a398e90db890a7ac8301440eb95240bc
Tran Le <axe3182003@gmail.com>
2023-06-07 20:22:01 +0700

    hide password and grant queries for Connect Manager

--- DesktopSQL/Login & Users for Group/GrantPermissions.sql
+++ DesktopSQL/Login & Users for Group/GrantPermissions.sql
@@ -6,4 +6,7 @@ GRANT SELECT ON UserMenu TO UserManager;
 GRANT SELECT ON UserMenu TO GroupManager;
 GRANT SELECT ON UserMenu TO ConnectManager;
 GRANT INSERT, DELETE ON UserMenu TO GroupManager;
+/*Neu trong comboBox bang du lieu cua Grant khong hien thi day du cac bang du lieu cua app thi thuc thi nhung dong duoi:
+GRANT SELECT ON Menu TO ConnectManager;
+GRANT SELECT ON Users TO ConnectManager; */
 
--- WinFormsApp6/WinFormsApp6/Forms/Form1.Designer.cs
+++ WinFormsApp6/WinFormsApp6/Forms/Form1.Designer.cs
@@ -73,6 +73,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(383, 31);
             this.textBox2.TabIndex = 4;
+            this.textBox2.PasswordChar = '*';
             // 
             // label3
             // 
